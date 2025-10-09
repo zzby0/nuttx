af8f0151e457c74514125300496b463b4a074044
guanyi3 <guanyi3@xiaomi.com>
2025-10-09 17:50:12 +0800

    drivers/devfreq: add const to devfreq_governor_s and devfreq_driver_s
    we do not hope the governor and driver in devfreq to be modified.
    
    Signed-off-by: guanyi3 <guanyi3@xiaomi.com>

--- include/nuttx/devfreq.h
+++ include/nuttx/devfreq.h
@@ -54,8 +54,8 @@ struct devfreq_s
   char name[NAME_MAX];
   struct list_node node;
 
-  FAR struct devfreq_governor_s *governor;
-  FAR struct devfreq_driver_s *driver;
+  FAR const struct devfreq_governor_s *governor;
+  FAR const struct devfreq_driver_s *driver;
 
   FAR const uint32_t *freq_table;
   FAR void *governor_data;
@@ -134,8 +134,8 @@ extern "C"
 
 FAR struct devfreq_s *devfreq_register(
                           FAR const char *name,
-                          FAR struct devfreq_governor_s *governor,
-                          FAR struct devfreq_driver_s *driver,
+                          FAR const struct devfreq_governor_s *governor,
+                          FAR const struct devfreq_driver_s *driver,
                           FAR void *priv);
 
 /****************************************************************************
